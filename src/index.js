c2a4543dd36adeb996f7656c0360ecc483d13557
Ekaterina Mikhailova <ekaterinavorobyova20@gmail.com>
2023-01-25 20:57:07 +0300

    part of brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const Brackets = [];
+    let j=0;
+  for (i=0; i<str.length; i++) {
+    if (str[i]=='(' || str[i]=='{' || str[i]=='[' || str[i]=='|') {
+      Brackets.push(str[i]);
+    }
+    else if (str[i]==')' || str[i]=='}' || str[i]==']') {
+     Brackets.pop();
+    }
+  }
+
+  for (elem of Brackets) {
+    if (elem=='|' ) {
+      j+=1;
+    }
+  }
+  if (i%2!==0) return false;
+  if (i%2==0 && Brackets.length%2==0) return true;
+
+  if (Brackets) return false
+  else return true;
 }
